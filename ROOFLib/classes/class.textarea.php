9ca70836ed760b8028e459a18703c58202a71533
Jeremie Jarosh <jeremie@jarosh.org>
2014-04-14 16:52:25 -0500

    Remove 'WYSIWYG' code
    'WYSIWYG' can be easily added via javascript if needed, therefore it
    shouldn't be in this code.

--- ROOFLib/classes/class.textarea.php
+++ ROOFLib/classes/class.textarea.php
@@ -25,23 +25,12 @@ class FI_TextArea extends FI_Text {
  */
 	public function __construct($name, $label, $options = Array()) {
 		parent::__construct($name, $label, $options);
-		global $__WYSIWYG_TEXTAREAS;
-		if (! $__WYSIWYG_TEXTAREAS) {
-			$__WYSIWYG_TEXTAREAS = Array();
-		}
-		if (! defined('WYSIWYG_SCRIPT')) {
-			define('WYSIWYG_SCRIPT', 'WYSIWYG_SCRIPT');
-		}
 		$defaultValues = Array(
 			'wysiwyg'=>false,
-			'wysiwygInputClass'=>'ckeditor',
-			'wysiwyg_global'=>'<script type="text/javascript" src="'.WYSIWYG_SCRIPT.'"></script>'
+			'wysiwygInputClass'=>'ckeditor'
 		);
 
 		$this->merge($options, $defaultValues);
-		if ($this->wysiwyg) {
-			$__WYSIWYG_TEXTAREAS []= $this->name().'_w';
-		}
 	}
 
 
@@ -60,15 +49,13 @@ class FI_TextArea extends FI_Text {
  *
  * @param Bool $wysiwyg Whether or not to use a WYSIWYG editor; Default:false
  * @param String $wysiwygInputClass The class to append to the textarea to identify it as a WYSIWYG; Default:'ckeditor'
- * @param String $wysiwyg_global The script tag to be added to include the WYSIWYG script; Default:'<script type="text/javascript" src="'.WYSIWYG_SCRIPT.'"></script>'
  *
  * @return Array The optional parameters which describe this class.
  */
 	public static function description () {
 		return Array(
 			'wysiwyg'=>self::DE('bool', 'Whether or not to use a WYSIWYG editor', 'false'),
-			'wysiwygInputClass'=>self::DE('string', 'The class to append to the textarea to identify it as a WYSIWYG', '\'ckeditor\''),
-			'wysiwyg_global'=>self::DE('string', 'The script tag to be added to include the WYSIWYG script', '\'<script type="text/javascript" src="\'.WYSIWYG_SCRIPT.\'"></script>\''),
+			'wysiwygInputClass'=>self::DE('string', 'The class to append to the textarea to identify it as a WYSIWYG', '\'ckeditor\'')
 		);
 	}
 
@@ -79,15 +66,7 @@ class FI_TextArea extends FI_Text {
  * @return String The HTML to be printed as a form.
  */
 	public function printForm() {
-		global $__WYSIWYG_TEXTAREAS, $__WYSIWYG_PRINT;
-		$script = '';
-		if ($this->wysiwyg && ! $__WYSIWYG_PRINT) {
-			$names = join(', ', $__WYSIWYG_TEXTAREAS);
-			$script = $this->wysiwyg_global;
-			$__WYSIWYG_PRINT = true;
-		}
-
-		return $script.'<textarea '.(($this->wysiwyg && $this->wysiwygInputClass)?('class="'.$this->wysiwygInputClass.'" '):'').'id="'.$this->name().'_w" cols="40" rows="4" '.(($this->required() && ($this->required_attr || $this->form->required_attr))?'required="required" ':'') .'name="'.$this->name().'">'.htmlentities($this->value()).'</textarea>'.$this->printDescription();
+		return '<textarea '.(($this->wysiwyg && $this->wysiwygInputClass)?('class="'.$this->wysiwygInputClass.'" '):'').'id="'.$this->name().'_w" cols="40" rows="4" '.(($this->required() && ($this->required_attr || $this->form->required_attr))?'required="required" ':'') .'name="'.$this->name().'">'.htmlentities($this->value()).'</textarea>'.$this->printDescription();
 	}
 
 
